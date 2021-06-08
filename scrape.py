58cb12407be32cd84c0f06eb80abc0f272233e9e
digitaluniverse <contact@mattdaniele.com>
2021-06-08 13:57:29 -0700

    update scrape

--- scrape.py
+++ scrape.py
@@ -7,11 +7,12 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 import time
 import re
+import webbrowser 
 browser = webdriver.Chrome('./chromedriver')
 browser.maximize_window()
-
+time.sleep(1)
 browser.get('https://www.aa.com/booking/find-flights')
-time.sleep(3)
+time.sleep(2)
 one_way = browser.find_element_by_id('tabOneWay')
 redeem = browser.find_element_by_id('awardBooking')
 
@@ -37,27 +38,29 @@ origin.clear()
 
 origin.send_keys('SFO')
 
-destination.send_keys('VCE')
+destination.send_keys('PAR')
 
 depart_date.send_keys('09/28/2021')
 
 travel_time.select_by_value('120001')
 
 passenger_count.select_by_value('2')
-business_first.select_by_value("BUSINESS_FIRST")
+# business_first.select_by_value("BUSINESS_FIRST")
 
-browser.execute_script('arguments[0].click()', redeem)
+# browser.execute_script('arguments[0].click()', redeem)
 
-browser.execute_script('arguments[0].click()', nearby_airport)
-
-
+# browser.execute_script('arguments[0].click()', nearby_airport)
+time.sleep(.5)
 search_button.click()
 browser.implicitly_wait(10)
+time.sleep(.5)
 
 
 try:
     element = WebDriverWait(browser, 40).until(
-        EC.presence_of_element_located((By.ID, "itinerary-qpxc-result-id"))
+        # EC.presence_of_element_located((By.ID, "itinerary-qpxc-result-id"))
+        EC.presence_of_element_located((By.ID, "showMoreLink"))
+
     )
 except TimeoutException:
     print ("Loading took too much time!")
@@ -75,8 +78,8 @@ try:
     try:
         for i in range(btnCount):
             browser.implicitly_wait(2)
+            time.sleep(.1)
             showMoreLink = browser.find_element_by_id('showMoreLink')
-
             showMoreLink.click()
             showingcount = browser.find_element_by_class_name('showingcount')
             print(showingcount.text)
@@ -86,13 +89,18 @@ except Exception as error:
     print(error)
     pass
 
-results = browser.find_element_by_class_name('results-desktop').get_attribute("innerHTML")
+# results = browser.find_element_by_class_name('results-desktop').get_attribute("innerHTML")
 
-# pageSource = browser.page_source
-# fileToWrite = open("index.html", "w")
-# fileToWrite.write(pageSource)
-# fileToWrite.close()
-# fileToRead = open("index.html", "r")
-# print(fileToRead.read())
-# fileToRead.close()
+pageSource = browser.page_source
 browser.quit()
+print('Generating Scraped Source')
+
+fileToWrite = open("www/public_html/index.html", "w")
+fileToWrite.write(pageSource)
+fileToWrite.close()
+fileToRead = open("www/public_html/index.html", "r")
+print(fileToRead.read())
+fileToRead.close()
+
+webbrowser.open_new("http://localhost:8080")
+
